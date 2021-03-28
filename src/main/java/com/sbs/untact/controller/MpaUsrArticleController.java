95c1dcbf0a26f3815aaca9eda06b2258bcb8ba75
rlatkdrud123@naver.com <rlatkdrud123@naver.com>
2021-03-28 16:38:55 +0900

    번호입력 시 번호에 맞는 글 출력

--- src/main/java/com/sbs/untact/controller/MpaUsrArticleController.java
+++ src/main/java/com/sbs/untact/controller/MpaUsrArticleController.java
@@ -1,5 +1,8 @@
 package com.sbs.untact.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,23 +18,60 @@ import com.sbs.untact.dto.ResultData;
 
 @Controller
 public class MpaUsrArticleController {
+
+	private int i;
+	private List<Article> articles;
 	
-	private int i=0;
-	
+	public MpaUsrArticleController() {
+		articles = new ArrayList<>();
+		i = 0;
+	}
+
 	@RequestMapping("/mpaUsr/article/doWrite")
 	@ResponseBody
 	public ResultData doWrite(String title, String body) {
-		
-		int id = i+1;
-		String regDate = Util.getNowDateStr();
-		String updateDate = Util.getNowDateStr();
-		Article article = new Article(id, regDate, updateDate, title, body);
-		i=id;
-			
+
+		int id = WriteArticle(title, body);
+		Article article = getArticleById(id);
+
 		return new ResultData("S-1", id + "번 글이 작성되었습니다.", "article", article);
 	}
 	
+	public int WriteArticle(String title, String body) {
+		int id = i + 1;
+		String regDate = Util.getNowDateStr();
+		String updateDate = Util.getNowDateStr();
+		Article article = new Article(id, regDate, updateDate, title, body);
+		articles.add(article);
+		i = id;
+		
+		return id;
+	}
 
+	@RequestMapping("/mpaUsr/article/getarticle")
+	@ResponseBody
+	public ResultData getArticle(int id) {
+		Article article = getArticleById(id);
+		
+		if(article==null) {
+			
+			return new ResultData("F-1", id + "번 글이 없습니다.", "id", id);
+		}
 
+		return new ResultData("S-1", id + "번 글입니다.", "article", article);
+
+	}
+
+	private Article getArticleById(int id) {
+
+		for (Article article : articles) {
+			if (article.getId() == id) {
+				return article;
+
+			}
+
+		}
+		return null;
+	}
 
 }
