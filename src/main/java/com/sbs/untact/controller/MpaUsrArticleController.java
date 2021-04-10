d092e5a07b2ad879a152bec33ab1974cebda0f07
rlatkdrud123@naver.com <rlatkdrud123@naver.com>
2021-04-10 13:04:17 +0900

    게시물누를때 이름도 나타내기

--- src/main/java/com/sbs/untact/controller/MpaUsrArticleController.java
+++ src/main/java/com/sbs/untact/controller/MpaUsrArticleController.java
@@ -1,23 +1,17 @@
 package com.sbs.untact.controller;
 
-import java.util.ArrayList;
-import java.util.List;
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.sbs.untactTeacher.util.Util;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
 
 import com.sbs.untact.Service.articleService;
 import com.sbs.untact.dto.Article;
 import com.sbs.untact.dto.Board;
 import com.sbs.untact.dto.ResultData;
+import com.sbs.untactTeacher.util.Util;
 
 @Controller
 public class MpaUsrArticleController {
@@ -33,73 +27,71 @@ public class MpaUsrArticleController {
 	@ResponseBody
 	public ResultData doWrite(String title, String body) {
 
-		if ( Util.isEmpty(title) ) {
+		if (Util.isEmpty(title)) {
 			return new ResultData("F-2", "제목을 입력해주세요.");
 		}
 
-		if ( Util.isEmpty(body) ) {
+		if (Util.isEmpty(body)) {
 			return new ResultData("F-3", "내용을 입력해주세요.");
 		}
-		
+
 		return ArticleService.writeArticle(title, body);
 	}
 
 	@RequestMapping("/mpaUsr/article/doDelete")
 	@ResponseBody
 	public ResultData doDelete(Integer id) {
-		
-		
-		if ( Util.isEmpty(id) ) {
+
+		if (Util.isEmpty(id)) {
 			return new ResultData("F-1", "번호를 입력해주세요.");
 		}
-		
+
 		return ArticleService.deleteArticleById(id);
 	}
-
 
 	@RequestMapping("/mpaUsr/article/doModify")
 	@ResponseBody
 	public ResultData doModify(Integer id, String title, String body) {
-		if ( Util.isEmpty(id) ) {
+		if (Util.isEmpty(id)) {
 			return new ResultData("F-1", "번호를 입력해주세요.");
 		}
 
-		if ( Util.isEmpty(title) ) {
+		if (Util.isEmpty(title)) {
 			return new ResultData("F-2", "제목을 입력해주세요.");
 		}
 
-		if ( Util.isEmpty(body) ) {
+		if (Util.isEmpty(body)) {
 			return new ResultData("F-3", "내용을 입력해주세요.");
 		}
-		
+
 		Article article = ArticleService.getArticleById(id);
-		
+
 		if (article == null) {
-			return  new ResultData("F-4", "존재하지 않는 게시물 번호입니다.");
+			return new ResultData("F-4", "존재하지 않는 게시물 번호입니다.");
 		}
 
 		return ArticleService.modifyArticle(id, title, body);
 	}
-	
+
 	@RequestMapping("/mpaUsr/article/list")
-	public String showlist(int boardId) {
-		
+	public String showList(HttpServletRequest req, int boardId) {
 		Board board = ArticleService.getBoardById(boardId);
 		
 		if(board == null) {
 			return "존재하지 않는 게시판입니다.";
 		}
 		
+		req.setAttribute("board", board);
 		return "mpaUsr/article/list";
 
+		
 	}
-
 
 	@RequestMapping("/mpaUsr/article/getarticle")
 	@ResponseBody
 	public ResultData getArticle(Integer id) {
-		
-		if ( Util.isEmpty(id) ) {
+
+		if (Util.isEmpty(id)) {
 			return new ResultData("F-1", "번호를 입력해주세요.");
 		}
 
@@ -113,7 +105,5 @@ public class MpaUsrArticleController {
 		return new ResultData("S-1", id + "번 글입니다.", "article", article);
 
 	}
-
-
 
 }
--- src/main/webapp/WEB-INF/jsp/mpaUsr/article/list.jsp
+++ src/main/webapp/WEB-INF/jsp/mpaUsr/article/list.jsp
@@ -3,7 +3,7 @@
 
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 
-<c:set var="pagetitle" value="<span><i class='far fa-clipboard'></i></span> <span>게시물 리스트</span>"/>
+<c:set var="pagetitle" value="<span><i class='far fa-clipboard'></i></span> <span>${board.name} 게시물 리스트 </span>"/>
 
 <%@ include file="../common/header.jspf" %>
 <%@ include file="../common/footer.jspf" %>
